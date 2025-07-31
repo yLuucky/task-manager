0f45664afa1870ccad9ebd5bcc1a2d12702e7dc4
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-07-31 11:31:22 -0300

    feat: Add IUserRepository interface with JPA integration and email lookup

---
+++ src/main/java/com/lucky/task_manager/user/domain/repositories/IUserRepository.java
@@ -0,0 +1,12 @@
+package com.lucky.task_manager.user.domain.repositories;
+
+import com.lucky.task_manager.user.domain.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
