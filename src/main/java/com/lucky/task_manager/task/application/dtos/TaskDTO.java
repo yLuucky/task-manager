5a0e0a029c559159c1489a580b8f253fd047d3c9
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-07-31 17:32:27 -0300

    feat: Add TaskDTO for task data transfer and validation

---
+++ src/main/java/com/lucky/task_manager/task/application/dtos/TaskDTO.java
@@ -0,0 +1,10 @@
+package com.lucky.task_manager.task.application.dtos;
+
+import com.lucky.task_manager.task.domain.enums.Status;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record TaskDTO(@NotNull UUID id, @NotNull String title, String description, Status status, Date createdAt, UUID userId) {
+}
