d87159ddaae61c35c1118a005382276098828c74
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 19:58:10 -0300

    feat: extend TaskController and TaskAPI with subtask management capabilities

--- src/main/java/com/lucky/task_manager/task/controllers/TaskAPI.java
+++ src/main/java/com/lucky/task_manager/task/controllers/TaskAPI.java
@@ -1,7 +1,10 @@
 package com.lucky.task_manager.task.controllers;
 
+import com.lucky.task_manager.task.application.dtos.SubTaskDTO;
+import com.lucky.task_manager.task.application.dtos.SubTaskResponse;
 import com.lucky.task_manager.task.application.dtos.TaskDTO;
 import com.lucky.task_manager.task.application.dtos.TaskResponse;
+import com.lucky.task_manager.task.application.exceptions.TaskAlreadyCompletedException;
 import com.lucky.task_manager.task.application.exceptions.TaskHasOpenSubTasksException;
 import com.lucky.task_manager.task.domain.enums.Status;
 import com.lucky.task_manager.user.application.exceptions.UserNotFoundException;
@@ -16,8 +19,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
+import static com.lucky.task_manager.task.application.exceptions.ITaskExceptions.SUBTASK_NOT_FOUND_EXCEPTION_MSG;
 import static com.lucky.task_manager.task.application.exceptions.ITaskExceptions.TASK_HAS_OPEN_SUBTASKS_EXCEPTION_MSG;
 import static com.lucky.task_manager.user.application.exceptions.IUserExceptions.USER_NOT_FOUND_EXCEPTION_MSG;
 
@@ -33,7 +38,7 @@ public interface TaskAPI {
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/tasks")
-    ResponseEntity<Void> createTask(TaskDTO taskDTO) throws UserNotFoundException;
+    ResponseEntity<TaskResponse> createTask(TaskDTO taskDTO) throws UserNotFoundException;
 
     @Operation(summary = "Retrieve a task", description = "Retrieve task by id and status")
     @ApiResponses(value = {
@@ -44,16 +49,48 @@ public interface TaskAPI {
     })
     @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/tasks")
-    ResponseEntity<TaskResponse> retrieveTask(@PathParam("status") Status status, int page);
+    ResponseEntity<List<TaskResponse>> retrieveTask(@PathParam("status") Status status, int page);
 
-    @Operation(summary = "Retrieve a task", description = "Update Task Status")
+    @Operation(summary = "Update Task Status", description = "Update Task Status")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Task successfully updated",
+            @ApiResponse(responseCode = "201", description = "Task status successfully updated",
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskResponse.class))),
             @ApiResponse(responseCode = "400", description = TASK_HAS_OPEN_SUBTASKS_EXCEPTION_MSG,
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     })
     @ResponseStatus(HttpStatus.CREATED)
-    @PutMapping("/tasks/{id}/status")
+    @PatchMapping("/tasks/{id}/status")
     ResponseEntity<Void> updateTask(@PathParam("id") UUID taskId, @PathParam("status") Status status) throws TaskHasOpenSubTasksException;
+
+    @Operation(summary = "Create a new subtask", description = "Create a new subtask on our system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Subtask successfully created",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = SubTaskDTO.class))),
+            @ApiResponse(responseCode = "400", description = TASK_HAS_OPEN_SUBTASKS_EXCEPTION_MSG,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping("/tasks/{id}/subtasks")
+    ResponseEntity<SubTaskResponse> createSubTask(@PathParam("taskId") UUID taskId, SubTaskDTO subTaskDTO) throws TaskAlreadyCompletedException;
+
+    @Operation(summary = "List all of subtasks from task", description = "List all of subtasks from task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "All subtask listed",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskResponse.class))),
+            @ApiResponse(responseCode = "400", description = SUBTASK_NOT_FOUND_EXCEPTION_MSG,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping("/tasks/{id}/subtasks")
+    ResponseEntity<List<SubTaskResponse>> retrieveAllTaskSubtasks(UUID taskId);
+
+    @Operation(summary = "Update Subtask Status", description = "Update SubTask Status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "SubTask status successfully updated",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskResponse.class))),
+            @ApiResponse(responseCode = "404", description = "SubTask not found",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    @PatchMapping("/subtasks/{id}/status")
+    ResponseEntity<Void> updateSubTask(@PathParam("subTaskId") UUID taskId, Status status) throws TaskAlreadyCompletedException;
 }
--- src/main/java/com/lucky/task_manager/task/controllers/TaskController.java
+++ src/main/java/com/lucky/task_manager/task/controllers/TaskController.java
@@ -1,17 +1,20 @@
 package com.lucky.task_manager.task.controllers;
 
+import com.lucky.task_manager.task.application.dtos.SubTaskDTO;
+import com.lucky.task_manager.task.application.dtos.SubTaskResponse;
 import com.lucky.task_manager.task.application.dtos.TaskDTO;
 import com.lucky.task_manager.task.application.dtos.TaskResponse;
+import com.lucky.task_manager.task.application.exceptions.TaskAlreadyCompletedException;
 import com.lucky.task_manager.task.application.exceptions.TaskHasOpenSubTasksException;
-import com.lucky.task_manager.task.application.services.ICreateTaskService;
-import com.lucky.task_manager.task.application.services.IRetrieveAllTasksByStatus;
-import com.lucky.task_manager.task.application.services.IUpdateTaskService;
+import com.lucky.task_manager.task.application.services.*;
 import com.lucky.task_manager.task.domain.enums.Status;
+import com.lucky.task_manager.task.domain.models.Task;
 import com.lucky.task_manager.user.application.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,34 +22,64 @@ public class TaskController implements TaskAPI{
 
     private final ICreateTaskService createTaskService;
     private final IUpdateTaskService updateTaskService;
-    private final IRetrieveAllTasksByStatus retrieveTaskServiceBy;
+    private final IRetrieveAllTasksByStatusService retrieveTaskServiceBy;
+    private final ICreateSubTaskService createSubTaskService;
+    private final IUpdateSubTaskService updateSubTaskService;
+    private final IRetrieveAllTaskSubTasksService retrieveAllTaskSubTasksService;
 
     @Autowired
     public TaskController(final ICreateTaskService createTaskService,
                           final IUpdateTaskService updateTaskService,
-                          final IRetrieveAllTasksByStatus retrieveTaskServiceBy) {
+                          final IRetrieveAllTasksByStatusService retrieveTaskServiceBy,
+                          final ICreateSubTaskService createSubTaskService,
+                          final IUpdateSubTaskService updateSubTaskService,
+                          final IRetrieveAllTaskSubTasksService retrieveAllTaskSubTasksService) {
         this.createTaskService = createTaskService;
         this.updateTaskService = updateTaskService;
         this.retrieveTaskServiceBy = retrieveTaskServiceBy;
+        this.createSubTaskService = createSubTaskService;
+        this.updateSubTaskService = updateSubTaskService;
+        this.retrieveAllTaskSubTasksService = retrieveAllTaskSubTasksService;
     }
 
     @Override
-    public ResponseEntity<Void> createTask(TaskDTO taskDTO) throws UserNotFoundException {
-        createTaskService.execute(taskDTO);
+    public ResponseEntity<TaskResponse> createTask(TaskDTO taskDTO) throws UserNotFoundException {
+        TaskResponse taskResponse = createTaskService.execute(taskDTO);
 
-        return ResponseEntity.status(201).build();
+        return ResponseEntity.status(201).body(taskResponse);
     }
 
     @Override
-    public ResponseEntity<TaskResponse> retrieveTask(final Status status, final int page) {
-        retrieveTaskServiceBy.execute(status, page);
+    public ResponseEntity<List<TaskResponse>> retrieveTask(final Status status, final int page) {
+        List<TaskResponse> taskResponse = retrieveTaskServiceBy.execute(status, page);
 
-        return ResponseEntity.status(201).build();
+        return ResponseEntity.status(200).body(taskResponse);
     }
 
     @Override
-    public ResponseEntity<Void> updateTask(UUID taskId, Status status) throws TaskHasOpenSubTasksException {
+    public ResponseEntity<Void> updateTask(final UUID taskId, final Status status) throws TaskHasOpenSubTasksException {
         updateTaskService.execute(taskId, status);
+
+        return ResponseEntity.status(200).build();
+    }
+
+    @Override
+    public ResponseEntity<SubTaskResponse> createSubTask(final UUID taskId, SubTaskDTO subTaskDTO) throws TaskAlreadyCompletedException {
+        SubTaskResponse subTaskResponse = createSubTaskService.execute(subTaskDTO, taskId);
+
+        return ResponseEntity.status(201).body(subTaskResponse);
+    }
+
+    @Override
+    public ResponseEntity<List<SubTaskResponse>> retrieveAllTaskSubtasks(final UUID taskId) {
+        List<SubTaskResponse> taskResponses = retrieveAllTaskSubTasksService.execute(taskId);
+
+        return ResponseEntity.status(200).body(taskResponses);
+    }
+
+    @Override
+    public ResponseEntity<Void> updateSubTask(final UUID taskId, final Status status) throws TaskAlreadyCompletedException {
+        updateSubTaskService.execute(taskId, status);
 
         return ResponseEntity.status(200).build();
     }
