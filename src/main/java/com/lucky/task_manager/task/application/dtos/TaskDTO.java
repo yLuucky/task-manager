06141fb0326981f93d3344f162af6ab03a51dc7d
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 11:35:50 -0300

    refactor: Replace Date with LocalDateTime in TaskDTO for consistency

--- src/main/java/com/lucky/task_manager/task/application/dtos/TaskDTO.java
+++ src/main/java/com/lucky/task_manager/task/application/dtos/TaskDTO.java
@@ -3,8 +3,8 @@ package com.lucky.task_manager.task.application.dtos;
 import com.lucky.task_manager.task.domain.enums.Status;
 import jakarta.validation.constraints.NotNull;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record TaskDTO(@NotNull UUID id, @NotNull String title, String description, Status status, Date createdAt, UUID userId) {
+public record TaskDTO(@NotNull UUID id, @NotNull String title, String description, Status status, LocalDateTime createdAt, @NotNull UUID userId) {
 }
