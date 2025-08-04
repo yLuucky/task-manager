61594b979b5eed03e3107573b5fc6b47929460df
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 11:36:01 -0300

    feat: Implement CreateSubTaskService and ICreateSubTaskService for subtask creation logic

---
+++ src/main/java/com/lucky/task_manager/task/application/services/ICreateSubTaskService.java
@@ -0,0 +1,11 @@
+package com.lucky.task_manager.task.application.services;
+
+import com.lucky.task_manager.task.application.dtos.SubTaskDTO;
+import com.lucky.task_manager.task.application.dtos.SubTaskResponse;
+import com.lucky.task_manager.task.application.exceptions.TaskAlreadyCompletedException;
+
+import java.util.UUID;
+
+public interface ICreateSubTaskService {
+    SubTaskResponse execute(SubTaskDTO taskDTO) throws TaskAlreadyCompletedException;
+}
---
+++ src/main/java/com/lucky/task_manager/task/application/services/Impl/CreateSubTaskService.java
@@ -0,0 +1,55 @@
+package com.lucky.task_manager.task.application.services.Impl;
+
+import com.lucky.task_manager.task.application.dtos.SubTaskDTO;
+import com.lucky.task_manager.task.application.dtos.SubTaskResponse;
+import com.lucky.task_manager.task.application.exceptions.TaskAlreadyCompletedException;
+import com.lucky.task_manager.task.application.services.ICreateSubTaskService;
+import com.lucky.task_manager.task.domain.enums.Status;
+import com.lucky.task_manager.task.domain.mappers.SubTaskMapper;
+import com.lucky.task_manager.task.domain.models.SubTask;
+import com.lucky.task_manager.task.domain.models.Task;
+import com.lucky.task_manager.task.domain.repositories.ISubTaskRepository;
+import com.lucky.task_manager.task.domain.repositories.ITaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static com.lucky.task_manager.task.application.exceptions.ITaskExceptions.TASK_ALREADY_COMPLETED_EXCEPTION;
+
+@Service
+public class CreateSubTaskService implements ICreateSubTaskService {
+
+    private final ITaskRepository ITaskRepository;
+    private final ISubTaskRepository ISubTaskRepository;
+    private final SubTaskMapper subTaskMapper;
+
+    @Autowired
+    public CreateSubTaskService(final ITaskRepository ITaskRepository,
+                                final ISubTaskRepository ISubTaskRepository,
+                                final SubTaskMapper subTaskMapper) {
+        this.ITaskRepository = ITaskRepository;
+        this.ISubTaskRepository = ISubTaskRepository;
+        this.subTaskMapper = subTaskMapper;
+    }
+
+    @Override
+    public SubTaskResponse execute(SubTaskDTO taskDTO) throws TaskAlreadyCompletedException {
+        final Task task = ITaskRepository.findById(taskDTO.taskId()).orElseThrow();
+
+        if (task.getStatus().equals(Status.CLOSED)) {
+            throw TASK_ALREADY_COMPLETED_EXCEPTION;
+        }
+
+        SubTask subTask = new SubTask();
+        subTask.setUserId(taskDTO.userId());
+        subTask.setTaskId(taskDTO.taskId());
+        subTask.setTitle(taskDTO.title());
+        subTask.setDescription(taskDTO.description());
+        subTask.setStatus(taskDTO.status());
+        subTask.setCreatedAt(LocalDateTime.now());
+
+        SubTask savedTask = ISubTaskRepository.save(subTask);
+        return subTaskMapper.toResponse(savedTask);
+    }
+}
