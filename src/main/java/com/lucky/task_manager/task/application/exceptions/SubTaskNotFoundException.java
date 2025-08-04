4a79ef835289e757183ee47eee4a3af7568a44d1
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 19:57:33 -0300

    feat: add SubTaskNotFoundException for handling missing subtasks

---
+++ src/main/java/com/lucky/task_manager/task/application/exceptions/SubTaskNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lucky.task_manager.task.application.exceptions;
+
+public class SubTaskNotFoundException extends Exception{
+    public SubTaskNotFoundException(String message) {
+        super(message);
+    }
+}
