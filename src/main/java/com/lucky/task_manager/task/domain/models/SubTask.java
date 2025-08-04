13d9b1f4380040f1d13921a3d98d8847d21127ce
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 11:35:45 -0300

    feat: Extend the SubTask entity with fields, relationships, and status handling

--- src/main/java/com/lucky/task_manager/task/domain/models/SubTask.java
+++ src/main/java/com/lucky/task_manager/task/domain/models/SubTask.java
@@ -1,11 +1,58 @@
 package com.lucky.task_manager.task.domain.models;
 
-import jakarta.persistence.Entity;
+import com.lucky.task_manager.task.domain.enums.Status;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@Table(name = "sub_tasks")
 public class SubTask extends Task {
+
+    @Id
+    @GeneratedValue(generator = "UUID4")
+    @UuidGenerator
+    private UUID subTaskId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status = Status.OPEN;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime concludedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    public void updateStatus(Status newStatus) {
+        this.status = newStatus;
+        if (newStatus.equals(Status.CLOSED)) {
+            this.concludedAt = LocalDateTime.now();
+        } else {
+            this.concludedAt = null;
+        }
+    }
+
+    public UUID getTaskId() {
+        return task != null ? task.getTaskId() : null;
+    }
 }
