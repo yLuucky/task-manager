508b9e63546e2e41183a698dfa0f0838cfc336e5
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 11:42:29 -0300

    feat: Add ITaskRepository for task persistence and query methods

---
+++ src/main/java/com/lucky/task_manager/task/domain/repositories/ITaskRepository.java
@@ -0,0 +1,20 @@
+package com.lucky.task_manager.task.domain.repositories;
+
+import com.lucky.task_manager.task.domain.enums.Status;
+import com.lucky.task_manager.task.domain.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ITaskRepository extends JpaRepository<Task, UUID> {
+
+    List<Task> findByUserIdAndStatus(UUID userId, Status status);
+    List<Task> findByUserId(UUID userId);
+    Optional<Task> findByIdWithSubTasks(@Param("taskId") UUID taskId);
+    boolean existsByTaskIdAndUserId(UUID taskId, UUID userId);
+}
