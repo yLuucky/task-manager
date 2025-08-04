9e439d1cf7c775365f7072b400c00b7caddc4bf8
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 11:35:36 -0300

    feat: Enhance Task entity with additional fields, relationships, and status management

--- src/main/java/com/lucky/task_manager/task/domain/models/Task.java
+++ src/main/java/com/lucky/task_manager/task/domain/models/Task.java
@@ -1,25 +1,69 @@
 package com.lucky.task_manager.task.domain.models;
 
+import com.lucky.task_manager.task.application.exceptions.TaskHasOpenSubTasksException;
 import com.lucky.task_manager.task.domain.enums.Status;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import static com.lucky.task_manager.task.application.exceptions.ITaskExceptions.TASK_HAS_OPEN_SUBTASKS_EXCEPTION;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@Table(name = "tasks")
 public class Task {
 
     @Id
+    @GeneratedValue(generator = "UUID4")
+    @UuidGenerator
     private UUID taskId;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
-    private Status status;
-    private Date createdAt;
-    private Date concludedAt;
+
+    private Status status = Status.OPEN;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime concludedAt;
+
+    @Column(nullable = false)
     private UUID userId;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<SubTask> subTasks = new ArrayList<>();
+
+    public void updateStatus(Status newStatus) throws TaskHasOpenSubTasksException {
+        if (newStatus.equals(Status.CLOSED)) {
+            this.completeTask();
+            return;
+        }
+        this.status = newStatus;
+        this.concludedAt = null;
+    }
+
+    private boolean canBeCompleted(){
+        return subTasks.isEmpty() || subTasks.stream().allMatch(subTask -> subTask.getStatus().equals(Status.CLOSED));
+    }
+
+    private void completeTask() throws TaskHasOpenSubTasksException {
+        if (!canBeCompleted()) throw TASK_HAS_OPEN_SUBTASKS_EXCEPTION;
+        this.status = Status.CLOSED;
+        this.concludedAt = LocalDateTime.now();
+    }
 }
