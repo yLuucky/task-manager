d24c512f0f0f19cf8039327c93c7946cdf360ba0
yLuucky <57505844+yLuucky@users.noreply.github.com>
2025-08-04 16:42:56 -0300

    feat: add TaskExceptionHandler class with @RestControllerAdvice annotation

---
+++ src/main/java/com/lucky/task_manager/task/controllers/handlers/TaskExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.lucky.task_manager.task.controllers.handlers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TaskExceptionHandler {
+}
